59174426983418a6dff0adbc306ebe85de20bf8c
jackr276 <jack_m_robbins@me.com>
2024-04-19 10:45:58 -0400

    rename, continue work

--- src/solve.c
+++ src/solve.c
@@ -17,9 +17,9 @@
 
 
 /**
- * Defines a structure that represents each state in the puzzle
+ * Defines a type of state, which is a structure, that represents a configuration 
  */
-struct state {
+typedef struct {
 	//There are 4x4 = 16 tiles in the game
 	int tiles[N][N];
 	//For A*, define the total_cost, how far the tile has traveled, and heuristic cost
@@ -30,20 +30,20 @@ struct state {
 	struct state* next;
 	//The predecessor of the current state, used for tracing back a solution	
 	struct state* predecessor;			
-};
+} state;
 
 
 /* The following global variables are defined for convenience */
 
 int goal_rows[NxN];
 int goal_columns[NxN];
-struct state* start;
-struct state* goal;
-struct state* fringe = NULL;
-struct state* *closed = NULL;
-struct state* succ_states[4];
+state* start;
+state* goal;
+state* fringe = NULL;
+state* closed = NULL;
+state* succ_states[4];
 
-void print_a_state(struct state *pstate) {
+void print_a_state(state *pstate) {
 	int i,j;
 	for (i=0;i<N;i++) {
 		for (j=0;j<N;j++) 
@@ -56,7 +56,7 @@ void print_a_state(struct state *pstate) {
 void initialize(char **argv){
 	int i,j,k,index, tile;
 
-	start=(struct state *) malloc(sizeof(struct state));
+	start=(state*)malloc(sizeof(state));
 	index = 1;
 	for (j=0;j<N;j++)
 		for (k=0;k<N;k++) {
@@ -76,7 +76,7 @@ void initialize(char **argv){
 	print_a_state(start);
 
 	//Initialize the goal state(all tiles in order, 0 at the very last one)
-	goal=(struct state*)malloc(sizeof(struct state));
+	goal=(state*)malloc(sizeof(state));
 	goal_rows[0]=3;
 	goal_columns[0]=3;
 
@@ -103,44 +103,128 @@ void initialize(char **argv){
 void merge_to_fringe() { 
 }
 
-/*swap two tiles in a state*/
-void swap(int row1,int column1,int row2,int column2, struct state * pstate){
-}
-
 /*update the f,g,h function values for a state pointed by a pointer in succ_states */
 void update_fgh(int i) {
-	struct state *pstate = succ_states[i];
-	...
+	state* pstate = succ_states[i];
+
+		
 }
 
-/* 0 goes down by a row */
-void move_down(struct state * pstate){
-}
 
-/* 0 goes right by a column */
-void move_right(struct state * pstate){
-}
-
-/* 0 goes up by a row */
-void move_up(struct state * pstate){
-}
-
-/* 0 goes left by a column */
-void move_left(struct state * pstate){
-}
-
-/* expand a state, and get its children states. Children states pointed by the pointers in succ_states.
+/**
+ * A simple function that swaps two tiles in the provided state
+ * Note: The swap function assumes all row positions are valid, this must be checked by the caller
  */
-void expand(struct state *selected) {
-	...
-	succ_states[..] = ...;
-	...
+void swap(int row1, int column1, int row2, int column2,state* statePtr){
+	//Store the first tile in a temp variable
+	int tile = statePtr->tiles[row1][column1];
+	//Put the tile from row2, column2 into row1, column1
+	statePtr->tiles[row1][column1] = statePtr->tiles[row2][column2];
+	//Put the temp in row2, column2
+	statePtr->tiles[row2][column2] = tile;
+}
+
+
+/**
+ * Move the 0 slider down by 1 row
+ */
+void move_down(state* statePtr){
+	//Utilize the swap function, move the zero_row down by 1
+	swap(statePtr->zero_row, statePtr->zero_column, statePtr->zero_row+1, statePtr->zero_column, statePtr);	
+	//Increment the zero_row to keep the position accurate
+	statePtr->zero_row++;
+}
+
+
+/**
+ * Move the 0 slider right by 1 column
+ */
+void move_right(state* statePtr){
+	//Utilize the swap function, move the zero_column right by 1
+	swap(statePtr->zero_row, statePtr->zero_column, statePtr->zero_row, statePtr->zero_column+1, statePtr);	
+	//Increment the zero_column to keep the position accurate
+	statePtr->zero_column++;
+}
+
+
+/**
+ * Move the 0 slider up by 1 row
+ */
+void move_up(state* statePtr){
+	//Utilize the swap function, move the zero_row up by 1
+	swap(statePtr->zero_row, statePtr->zero_column, statePtr->zero_row-1, statePtr->zero_column, statePtr);	
+	//Decrement the zero_row to keep the position accurate
+	statePtr->zero_row--;
+}
+
+
+/**
+ * Move the 0 slider left by 1 column
+ */
+void move_left(state* statePtr){
+	//Utilize the swap function, move the zero_column left by 1
+	swap(statePtr->zero_row, statePtr->zero_column, statePtr->zero_row , statePtr->zero_column-1, statePtr);	
+	//Decrement the zero_column to keep the position accurate
+	statePtr->zero_column--;
+}
+
+
+/**
+ * This function generates all possible successors to a state and stores them in the successor array
+ * Note: 4 successors are not always possible, if a successor isn't possible, NULL will be put in its place 
+ */
+void generate_successors(state* predecessor){
+	//Create four pointers, one for each possible move, and initialize to NULL by default
+	state* leftMove = NULL;
+	state* rightMove = NULL;
+	state* upMove = NULL;
+	state* downMove = NULL;
+	
+	//Generate successor by moving left one if possible
+	if(predecessor->zero_column > 0){
+		//Create a new state
+		leftMove = (state*)malloc(sizeof(state));
+		//Move right by one
+		move_left(leftMove);
+	}
+	//Put leftMove into the array
+	succ_states[0] = leftMove;
+
+	//Generate successor by moving right one if possible
+	if(predecessor->zero_column < N-1){
+		//Create a new state
+		rightMove = (state*)malloc(sizeof(state));
+		//Move right by one
+		move_right(rightMove);
+	}
+	//Put rightMove into the array
+	succ_states[1] = rightMove;
+	
+	//Generate successor by moving down one if possible
+	if(predecessor->zero_row < N-1){
+		//Create a new state
+		downMove = (state*)malloc(sizeof(state));
+		//Move down by one
+		move_down(downMove);
+	}
+	//Put downMove into the array
+	succ_states[2] = downMove;
+	
+	//Generate successor by moving down one if possible
+	if(predecessor->zero_row > 0){
+		//Create a new state
+		upMove = (state*)malloc(sizeof(state));
+		//Move up by one
+		move_up(upMove);
+	}
+	//Put upMove into the array
+	succ_states[3] = upMove;
 }
 
 /**
  * Utilize memcp to compare two states, return true if they are the same
  */
-int states_same(struct state *a,struct state *b) {
+int states_same(state* a,state* b) {
 	int flg=FALSE;
 	if (memcmp(a->tiles, b->tiles, sizeof(int)*NxN) == 0)
 		flg=TRUE;
@@ -158,7 +242,7 @@ void filter(int i, struct state *pstate_list){
 
 int main(int argc,char **argv) {
 	int iter,cnt;
-	struct state *curr_state, *cp, *solution_path;
+	state *curr_state, *cp, *solution_path;
 	int ret, i, pathlen=0, index[N-1];
 
 	solution_path=NULL;
