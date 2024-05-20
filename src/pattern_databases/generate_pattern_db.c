d9a87cb6120f174b6ccfe33727262c2a82ab84c5
jackr276 <jack_m_robbins@me.com>
2024-05-20 17:12:45 -0400

    initial concept working

--- src/pattern_databases/generate_pattern_db.c
+++ src/pattern_databases/generate_pattern_db.c
@@ -5,6 +5,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 
 //Define a global variable for the value of N
@@ -25,11 +26,8 @@ struct pattern_cost{
 };
 
 
-//We will start at the goal and generate backwards
-struct simplified_state* goal_state;
-
 //We will keep a linked list of all patterns seen to avoid storing repeats
-struct pattern_cost* patterns;
+struct pattern_cost* patterns = NULL;
 
 
 /**
@@ -60,14 +58,6 @@ void create_goal_state(struct simplified_state* initial){
 
 	//Zero row and zero column are both N-1
 	initial->zero_row = initial->zero_column = N-1;
-
-
-	for(int i = 0; i<N; i++){
-		for(int j = 0; j<N; j++){
-			printf("%2d ", initial->tiles[i][j]);
-		}
-		printf("\n");
-	}
 }
 
 
@@ -90,6 +80,194 @@ void destroy_state(struct simplified_state* statePtr){
 }
 
 
+/**
+ * Define a simple function that checks if two states are identical for use in contained_in_db
+ */
+int states_same(struct simplified_state* statePtr1, struct simplified_state* statePtr2){
+	//Compare tile by tile
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			//If we find any difference, return false
+			if(statePtr1->tiles[i][j] != statePtr2->tiles[i][j]){
+				return 0;
+			}	
+		}
+	}
+	
+	//Return 1 because they are the same
+	return 1;
+}
+
+
+/**
+ * This function stores the pattern in a temporary linked list in memory. For simplicity,
+ * we will first store all patterns in memory, and then write this linked list to a database
+ * file after everything has been made
+ */
+int store_pattern(struct pattern_cost* patternPtr){
+	//Define a cursor to avoid messing with linked list head
+	struct pattern_cost* cursor = patterns;	
+
+	//Special case, adding at head
+	if(patterns == NULL){
+		patterns = patternPtr;
+		return 0;
+	}
+
+	//Iterate through the entire linked list
+	while(cursor->next != NULL){
+		if(states_same(cursor->pattern, patternPtr->pattern)){
+			//Two options here
+			if(patternPtr->cost < cursor->cost){
+				//We must always store the lowest possible cost, so change it if we find this
+				cursor->cost = patternPtr->cost;
+			}
+			
+			//PatternPtr was a repeat, and is useless
+			//Free the statePtr in patternPtr
+			destroy_state(patternPtr->pattern);
+			//Free the pointer
+			free(patternPtr);
+			//Set to NULL as a warning
+			patternPtr = NULL;
+			//They matched, so return true
+			return 1;
+		}
+	
+		//Iterate over linked list
+		cursor = cursor->next;
+	}
+
+	//If we get here, we've reached the tail and we didn't find a repeat, so store patternPtr
+	cursor->next = patternPtr;
+	//If we get here, its unique so return false
+	return 0;
+}
+
+
+/**
+ * A simple function that swaps two tiles in the provided state
+ * Note: The swap function assumes all row positions are valid, this must be checked by the caller
+ */
+void swap(int row1, int column1, int row2, int column2, struct simplified_state* statePtr){
+	//Store the first tile in a temp variable
+	int tile = statePtr->tiles[row1][column1];
+	//Put the tile from row2, column2 into row1, column1
+	statePtr->tiles[row1][column1] = statePtr->tiles[row2][column2];
+	//Put the temp in row2, column2
+	statePtr->tiles[row2][column2] = tile;
+}
+
+
+/**
+ * Move the 0 slider down by 1 row
+ */
+void move_down(struct simplified_state* statePtr){
+	//Utilize the swap function, move the zero_row down by 1
+	swap(statePtr->zero_row, statePtr->zero_column, statePtr->zero_row+1, statePtr->zero_column, statePtr);	
+	//Increment the zero_row to keep the position accurate
+	statePtr->zero_row++;
+}
+
+
+/**
+ * Move the 0 slider right by 1 column
+ */
+void move_right(struct simplified_state* statePtr){
+	//Utilize the swap function, move the zero_column right by 1
+	swap(statePtr->zero_row, statePtr->zero_column, statePtr->zero_row, statePtr->zero_column+1, statePtr);	
+	//Increment the zero_column to keep the position accurate
+	statePtr->zero_column++;
+}
+
+
+/**
+ * Move the 0 slider up by 1 row
+ */
+void move_up(struct simplified_state* statePtr){
+	//Utilize the swap function, move the zero_row up by 1
+	swap(statePtr->zero_row, statePtr->zero_column, statePtr->zero_row-1, statePtr->zero_column, statePtr);	
+	//Decrement the zero_row to keep the position accurate
+	statePtr->zero_row--;
+}
+
+
+/**
+ * Move the 0 slider left by 1 column
+ */
+void move_left(struct simplified_state* statePtr){
+	//Utilize the swap function, move the zero_column left by 1
+	swap(statePtr->zero_row, statePtr->zero_column, statePtr->zero_row , statePtr->zero_column-1, statePtr);	
+	//Decrement the zero_column to keep the position accurate
+	statePtr->zero_column--;
+}
+
+
+
+/**
+ * Generate patterns back to a certain traceback_depth
+ */
+void generate_patterns(int traceback_depth){
+	/**
+	 * From before, N puzzles of a depth less than 30 seem easy to solve quickly. So, for our generation, we will
+	 * generate states with moves starting from 30 and up to the traceback_depth
+	 */
+	//Set the seed for random generation
+	srand(time(NULL));
+	int random_move;
+
+	for(int max_moves = 30; max_moves < traceback_depth; max_moves++){
+		//For each depth, generate 100 states of that depth
+		for(int j = 0; j < 100; j++){
+			//Always start at the goal state and work backwards
+			struct simplified_state* start = malloc(sizeof(struct simplified_state));
+			//Generate goal state mathematically
+			create_goal_state(start);
+
+			//Create the pattern_cost struct
+			struct pattern_cost* pc = malloc(sizeof(struct pattern_cost));
+			//Point the pattern of pc to the newly made start state
+			pc->pattern = start;
+			//We are at the goal state, so no cost yet
+			pc->cost = 0;
+
+			//Now to generate a pattern randomly, perform i random moves on start
+			for(int moves = 0; moves < max_moves; moves++){
+				//Get a random move between 0 and 3
+				random_move = rand() % 4;
+		
+				//Every time we successfully make a random move, we've moved one more away from the goal, so increment cost
+				//Move left if possible and random_move is 0
+				if(random_move == 0 && start->zero_column > 0){
+					move_left(start);
+					pc->cost++;
+				}
+
+				//Move right if possible and random move is 1
+				if(random_move == 1 && start->zero_column < N-1){
+					move_right(start);
+					pc->cost++;
+				}
+
+				//Move down if possible and random move is 2
+				if(random_move == 2 && start->zero_row < N-1){
+					move_down(start);
+					pc->cost++;
+				}
+
+				//Move up if possible and random move is 3
+				if(random_move == 3 && start->zero_row > 0){
+					move_up(start);
+					pc->cost++;
+				}
+			}
+			//Save the pattern in memory if it is not a repeat
+			store_pattern(pc);	
+		}
+	}
+}
+
+
 /********************************************************************************************
  * Basic Idea: Store some pattern and how many moves it takes to go from that pattern to goal
  * Encoding Format -> attempt to minimize space usage
@@ -108,17 +286,7 @@ void destroy_state(struct simplified_state* statePtr){
 
  *******************************************************************************************/
 
-int contained_in_db(struct simplified_state* statePtr){
-	
 
-	return 0;
-}
-
-
-
-void generate_patterns(FILE* db, struct simplified_state* statePtr, int traceback_depth){
-		
-}
 
 
 int main(int argc, char** argv){
@@ -126,6 +294,7 @@ int main(int argc, char** argv){
 	if(argc < 3){
 		printf("Incorrect number of program arguments.\n");
 		printf("Usage: ./generate_pattern_db <N> <G>\n Where <N> is the row/column number of the N puzzle, <G> is group size\n");
+		return 0;
 	}
 
 	//Which group size do we want to generate patterns for
@@ -135,23 +304,31 @@ int main(int argc, char** argv){
 	if(sscanf(argv[1], "%d", &N) != 1 || sscanf(argv[2], "%d", &group_size) != 1){	
 		printf("Incorrect type of program arguments.\n");
 		printf("Usage: ./generate_pattern_db <N>\n Where <N> is the row/column number of the N puzzle, <G> is group size\n");
+		return 0;
 	}
+
+	//We won't was time generating databases for 3 puzzles, N must be more than 3 for the computational cost to be worth it
+	if(N < 4){
+		printf("It is entirely unnecessary to use a pattern database for a 3 X 3 puzzle. Now exiting. \n");
+		return 0;
+	}
+
 
 	//Filename is always of format "N_G.patterndb"
 	char db_filename[14];
 
+	//Test
+	generate_patterns(80);
+
 	//Save the filename into a string
-	sprintf(db_filename, "%d_%d.patterndb", N, group_size);
+	//sprintf(db_filename, "%d_%d.patterndb", N, group_size);
 
-	printf("Generating database: %s\n\n", db_filename);
+	//printf("Generating database: %s\n\n", db_filename);
 
-	FILE* db = fopen(db_filename, "w");	
+	//FILE* db = fopen(db_filename, "w");	
 
 	//Be sure to close the file
-	fclose(db);
-	goal_state = (struct simplified_state*)malloc(sizeof(struct simplified_state));
-	create_goal_state(goal_state);
-	destroy_state(goal_state);
+	//fclose(db);
 
 	return 0;
 }
