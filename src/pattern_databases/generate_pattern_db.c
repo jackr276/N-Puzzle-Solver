95fa4f24fc82c1bbd6ff17b7ebd8b6be41f4e6b9
jackr276 <jack_m_robbins@me.com>
2024-05-20 12:20:11 -0400

    continue work on pattern DB

--- src/pattern_databases/generate_pattern_db.c
+++ src/pattern_databases/generate_pattern_db.c
@@ -20,9 +20,52 @@ struct simplified_state{
 //We will start at the goal and generate backwards
 struct simplified_state* goal_state;
 
+void create_goal_state(struct simplified_state* initial){
+	initial->tiles = malloc(sizeof(int*)*N);
+
+	for(int i = 0; i < N; i++){
+		initial->tiles[i] = malloc(sizeof(int) * N);
+	}
+
+	int row, col;
+	for(int i = 0; i < N * N; i++){
+		row = i / N;
+		col = i % N;
+		initial->tiles[row][col] = i+1;
+	}
+
+	initial->tiles[N-1][N-1] = 0;
+
+	for(int i = 0; i<N; i++){
+		for(int j = 0; j<N; j++){
+			printf("%2d ", initial->tiles[i][j]);
+		}
+		printf("\n");
+	}
+
+
+}
+
+/********************************************************************************************
+ * Basic Idea: Store some pattern and how many moves it takes to go from that pattern to goal
+ * Encoding Format -> attempt to minimize space usage
+ * Idea: there are 16 positions on the board, numbers are 0-15
+ * --- NUMBER POSITION -----
+ *   	ROW_POSITION . . . .   MOVES_FROM_GOAL 
+ * With this, we can identify the 
+ *
+ * So for Example:
+ * 1  2  3  0
+ * 5  6  7  4
+ * 9  10 11 8
+ * 13 14 15 12
+ * Can be encoded as
+ * 1 2 3 0 5 6 7 4 9 10 11 8 13 14 15 12 3, where 3 is how many moves from goal this state is 
+
+ *******************************************************************************************/
 
 void generate_patterns(FILE* db, struct simplified_state* statePtr, int group_size){
-	
+
 }
 
 
@@ -30,10 +73,6 @@ int contained_in_db(FILE* db, struct simplified_state* statePtr){
 
 
 	return 0;
-}
-
-void example(char* input){
-	printf("Input was: %s\n", input);
 }
 
 
@@ -58,12 +97,13 @@ int main(int argc, char** argv){
 
 	sprintf(db_filename, "%d_%d.patterndb", N, group_size);
 
-	printf("%s\n", db_filename);
+	printf("Generating database: %s\n", db_filename);
 
 	FILE* db = fopen(db_filename, "a+");	
-	fprintf(db, "I work");
 
 	fclose(db);
+	goal_state = (struct simplified_state*)malloc(sizeof(struct simplified_state));
+	create_goal_state(goal_state);
 
 	return 0;
 }
