6cdf81c5e144317201ddfb1396cabeba760ccbe6
jackr276 <jack_m_robbins@me.com>
2024-05-27 08:51:48 -0400

    start config generation working in java

--- src/java_version/Main.java
+++ src/java_version/Main.java
@@ -1,6 +1,12 @@
+import puzzle.Puzzle;
 
 public class Main{
 	public static void main(String[] args){
-		System.out.println("I work");
+		Puzzle p = new Puzzle(4);
+		System.out.println(p.toString());
+		System.out.println("Generating config");
+
+		p.createStartConfig(200);
+		System.out.println(p.toString());
 	}
 }
--- src/java_version/puzzle/Puzzle.java
+++ src/java_version/puzzle/Puzzle.java
@@ -11,60 +11,144 @@ import java.util.Random;
 public class Puzzle{
 	//The size of the row by column dimension
 	private int N;
+	//Use a 2d array for the puzzle
 	private short[][] puzzle;
+	//Keep track of the zero row and zero column
+	private short zeroRow, zeroColumn;
+	/* Heuristic function values */
+	private int currentTravel;
+	private int heuristicValue;
+	private int predictionValue;
+	/* ========================= */
+
 
 	//The puzzle constructor takes in a number N for the row and column dimensions of the puzzle
 	public Puzzle(int N){
-		if(N < 3){
-		}
-
-		//If it is good set the value
 		this.N = N;	
 		//Reserve the appropraite space for the puzzle
 		puzzle = new short[N][N];
+		//Call the helper to create the goal config
+		this.createGoalConfig();
 	}
 
+
+	/**
+	 * A simple private helper method that mathematically generates the start config for any given N
+	 */
+	private void createGoalConfig(){
+		short row, col;
+		//Go through each tile, mathematically finding position
+		for(short tile = 1; tile < this.N * this.N; tile++){
+			row = (short)((tile - 1) / this.N);	
+			col = (short)((tile - 1) % this.N);
+			//Store the tile value in the proper position
+			this.puzzle[row][col] = tile;
+		}
+		//Zero row and column always start out as the last
+		this.zeroRow = this.zeroColumn = (short)(N-1);
+		//Save in the zero slider
+		this.puzzle[zeroRow][zeroColumn] = 0;
+	}
 
 
 	public void createStartConfig(int maxMoves){
-		Random rand = new Random(4);
+		Random rand = new Random(1000000000);
 		//Get a random integer for our random move
-		int direction = rand.nextInt(0, 3); 
+		int direction; 
 	
 		//Randomly move the puzzle around for complexity moves
-		for(int move = 0; move < maxMoves; move++)
+		for(int move = 0; move < maxMoves; move++){
+			//Get the next random move value
+			direction = rand.nextInt() % 4;
 
+			//0 = left move
+			if(direction == 0 && this.zeroColumn > 0){
+				this.moveLeft();
+			}
+
+			//1 = right move
+			if(direction == 1 && this.zeroColumn < N - 1){
+				this.moveRight();
+			}
+			
+			//2 = down move
+			if(direction == 2 && this.zeroRow < N - 1){
+				this.moveDown();
+			}
+
+			//3 = up move
+			if(direction == 3 && this.zeroRow > 0){
+				this.moveUp();
+			}
+		}
 	}
 
-	private void swap(){
-
+	
+	/**
+	 * A simple helper function that swaps two given tiles
+	 *
+	 * Precondition: all row and column positions have been checked and are valid
+	 */
+	private void swap(int row1, int column1, int row2, int column2){
+		short tile = this.puzzle[row1][column1];
+		this.puzzle[row1][column1] = this.puzzle[row2][column2];
+		this.puzzle[row2][column2] = tile;
 	}
 
+
+	/**
+	 * A helper function that swaps the zero slider with its leftmost neighbor
+	 */
 	private void moveLeft(){
-
+		swap(this.zeroRow, this.zeroColumn, this.zeroRow, this.zeroColumn - 1);
+		//Keep position up to date
+		this.zeroColumn--;
 	}
 
+
+	/**
+	 * A helper function that swaps the zero slider with its rightmost neighbor
+	 */
 	private void moveRight(){
-
+		swap(this.zeroRow, this.zeroColumn, this.zeroRow, this.zeroColumn + 1);
+		//Keep position up to date
+		this.zeroColumn++;
 	}
 
+
+	/**
+	 * A helper function that swaps the zero slider with its upper neighbor
+	 */
 	private void moveUp(){
-
+		swap(this.zeroRow, this.zeroColumn, this.zeroRow - 1, this.zeroColumn);
+		//Keep position up to date
+		this.zeroRow--;
 	}
 
+
+	/**
+	 * A helper function that swaps the zero slider with its lower neighbor
+	 */
 	private void moveDown(){
-
+		swap(this.zeroRow, this.zeroColumn, this.zeroRow + 1, this.zeroColumn);
+		//Keep position up to date
+		this.zeroRow++;
 	}
 
 
+	/**
+	 * Simple toString() that prints out the puzzle board
+	 */
 	@Override
 	public String toString(){
 		String retVal = "";
-
+		//Print out tile by tile
 		for(int i = 0; i < N; i++){
 			for(int j = 0; j < N; j++){
-				retVal += this.puzzle[i][j] + " ";
+				//Use string format for spacing
+				retVal += String.format("%2d ", this.puzzle[i][j]);
 			}
+			//Give the effect of rows with newline
 			retVal += "\n";
 		}
 	
