b3b224fb22ee5b92a717c6abaaa92f9c0df935c7
jackr276 <jack_m_robbins@me.com>
2024-06-01 17:12:31 -0400

    initial refactor complete

--- src/solve.c
+++ src/solve.c
@@ -7,13 +7,9 @@
  * Note: This is the single-threaded version of the solver
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 //For timing
 #include <time.h>
 #include "puzzle.h"
-
 
 
 /**
@@ -86,119 +82,37 @@ void generate_successors(struct state* predecessor, struct state* successors[4],
 
 
 /**
- * A simple helper function that will tell if two states are the same. To be used for filtering
- */
-int states_same(struct state* a, struct state* b){
-	//Efficiency speedup -- if zero row and column aren't equal, return false
-	if(a->zero_row != b->zero_row || a->zero_column != b->zero_column){
-		return 0;
-	}
-
-	//Go through each row in the dynamic tile matrix in both states
-	for(int i = 0; i < N; i++){
-		//We can use memcmp to efficiently compare the space pointed to by each pointer
-		if(memcmp(a->tiles[i], b->tiles[i], sizeof(short) * N) != 0){
-			//If we find a difference, return 0
-			return 0;
-		}
-	}
-
-	//Return 1 if same	
-	return 1;
-}
-
-
-/**
- * Check to see if the state at position i in the fringe is repeating. If it is, free it and
- * set the pointer to be null
- */
-void check_repeating_fringe(int i){ 	
-	//If succ_states[i] is NULL, no need to check anything
-	if(succ_states[i] == NULL){
-		return;
-	}
-
-	//Get a cursor to iterate over the linkedList	
-	struct state* cursor = fringe;
-	//Go through the linkedList, if we ever find an element that's the same, break out and free the pointer
-	while(cursor != NULL){
-		//If the states match, we free the pointer and exit the loop
-		if(states_same(succ_states[i], cursor)){
-			//Properly tear down the dynamic array in the state to avoid memory leaks
-			destroy_state(succ_states[i]);
-			//Free the pointer to the state
-			free(succ_states[i]);
-			//Set the pointer to be null as a warning
-			succ_states[i] = NULL;
-			break;
-		}
-		//Move to the next state in the linkedList
-		cursor = cursor->next;
-		//Once we get here, the state at i either survived and isn't null, or was freed and set to NULL
-	}
-}
-
-
-/**
- * Check for repeats in the closed array. Since we don't need any priority queue functionality,
- * using closed as an array is a major speedup for us
- */
-void check_repeating_closed(int max_index, int state_index){
-	//If this has already been made null, simply return
-	if(succ_states[state_index] == NULL){
-		return;
-	}
-
-	//Go through the entire populated closed array
-	for(int i = max_index - 1; i > -1; i--){
-		//If at any point we find that the states are the same
-		if(states_same(closed[i], succ_states[state_index])){
-			//Free both the internal memory and the state pointer itself
-			destroy_state(succ_states[state_index]);
-			free(succ_states[state_index]);
-			//Set to null as a warning
-			succ_states[state_index] = NULL;
-			//Break out of the loop and exit
-			break;
-		}
-	}
-	//If we get here, we know that the state was not repeating
-}
-
-
-/**
  * Use an A* search algorithm to solve the 15-puzzle problem by implementing the A* main loop. If the solve function 
  * is successful, it will print the resulting solution path to the console as well.  
  */
-int solve(int* N){
+int solve(int N, struct state* start_state, struct state* goal_state){
 	//Get the CPU clock start time
 	clock_t begin_CPU = clock();
 
 	//We will keep track of the number of iterations as a sanity check for large problems
-	int iter = 0;
+	int iteration = 0;
 	//Keep track of the number of unique configurations made
 	int num_unique_configs = 0;
+	//Define an array for holding successor states. We can generate at most 4 each time
+	struct state* successors[4];
+
 	//Put the start state into the fringe to begin the search
-	fringe = start_state; 
-	//We will keep a reference to the next available closed index	
-	int next_closed_index = 0;
-	//Make the initial allocation for closed
-	closed = malloc(sizeof(struct state*) * closed_max_size);
+	priority_queue_insert(start_state);
+
+	//Initialize the closed list
+	initialize_closed();
 
 	//Maintain a pointer for the current state in the search
 	struct state* curr_state;
 
-	struct state* succ_states[4];
-
 	//Algorithm main loop -- while there are still states to be expanded, keep iterating until we find a solution
-	while (fringe != NULL){
+	while (!fringe_empty()){
 		//Remove or "pop" the head of the fringe linked list -- because fringe is a priority queue, this is the most
 		//promising state to explore next
-		curr_state=fringe;
-		fringe=fringe->next;
+		curr_state = dequeue();
 
 		//Check to see if we have found the solution. If we did, we will print out the solution path and stop
-		if(states_same(curr_state, goal_state)){
+		if(states_same(curr_state, goal_state, N)){
 			//Stop the clock if we find solution
 			clock_t end_CPU = clock();
 
@@ -249,49 +163,38 @@ int solve(int* N){
 		}
 
 		//Generate successors to the current state once we know it isn't a solution
-		generate_successors(curr_state);
+		generate_successors(curr_state, successors, N);
 
 		//Go through each of the successor states, and check for repetition/update prediction function
 		for(int i = 0; i < 4; i++){
 			//If the state is already null, there is no point in further exploration
-			if(succ_states[i] == NULL){
+			if(successors[i] == NULL){
 				continue;
 			}
 
 			//Check each successor state against fringe and closed to see if it is repeating
 			//Check the current state in the closed array
-			check_repeating_closed(next_closed_index, i);
+			check_repeating_closed(successors[i], N);
 			//Check against fringe
-			check_repeating_fringe(i); 
+			check_repeating_fringe(successors[i], N);
 			//Update the prediction function on states that don't repeat
-			update_prediction_function(i); 
+			update_prediction_function(successors[i], N); 
 		}
 	
 		//Add all necessary states to fringe now that we have checked for repeats and updated predictions 
 		//Additionally, we need to update the num_unique_configs, this will be done in merge_to_fringe 
-		num_unique_configs += merge_to_fringe(succ_states); 
+		num_unique_configs += merge_to_fringe(successors); 
 
-		//Add to closed
-		//If we run out of space, we can expand
-		if(next_closed_index == closed_max_size){
-			//Double closed max size
-			closed_max_size *= 2;
-			//Reallocate space for closed
-			closed = realloc(closed, sizeof(struct state*) * closed_max_size);
-		}
-
-		//Put curr_state into closed
-		closed[next_closed_index] = curr_state;
-		//Keep track of the next closed index
-		next_closed_index++;
+		//Merge the current state into closed
+		merge_to_closed(curr_state);
 
 		//For very complex problems, print the iteration count to the console for a sanity check
-		if(iter > 1 && iter % 1000 == 0) {
-			printf("Iteration: %6d, %6ld total unique states generated\n", iter, num_unique_configs);
+		if(iteration > 1 && iteration % 1000 == 0) {
+			printf("Iteration: %6d, %6d total unique states generated\n", iteration, num_unique_configs);
 		}
 		
 		//End of one full iteration
-		iter++;
+		iteration++;
 	}
 	
 	//If we end up here, fringe became NULL with no goal configuration found, so there is no solution
@@ -328,9 +231,13 @@ int main(int argc, char** argv){
 	//Important: Move the address of argv up by 1 so that initialize_start_goal can only see the initial config 
 	argv += 1;
 
+	//The starting and goal states
+	struct state* start_state;
+	struct state* goal_state;
+
 	//Initialize the goal and start states 
-	initialize_start_goal(argv, &N);
+	initialize_start_goal(argv, start_state, goal_state, N);
 
 	//Call the solve() funciton and hand off the rest of the program execution to it
-	return solve(&N);
+	return solve(N, start_state, goal_state);
 }
